9ea26434661351681fa19cbd2b9f61e507268e7f
HEYAHONG <2229388563@qq.com>
2025-01-23 17:44:45 +0800

    优化清理脚本

--- FW/C/001-base/clean.bat
+++ FW/C/001-base/clean.bat
@@ -3,6 +3,33 @@
 @rem 目录
 set APP_ROOT_PATH=%~dp0
 
-if exist "%APP_ROOT_PATH%\..\sdk\clean.bat" call "%APP_ROOT_PATH%\..\sdk\clean.bat"
-if exist "%APP_ROOT_PATH%\..\sdk\out" del /s /q "%APP_ROOT_PATH%\..\sdk\out"
-if exist "%APP_ROOT_PATH%\..\sdk\build" del /s /q "%APP_ROOT_PATH%\..\sdk\build"
+@rem 设置sdk所需环境变量设置(可根据实际情况设置环境变量)
+set PROJECT_NAME=ExtBoard-002-001-base
+set PROJECT_DIR=%APP_ROOT_PATH%\
+set PROJECT_DIR=%PROJECT_DIR:\=/%
+if "X" == "X%LSPD_MODE%" set LSPD_MODE=disable
+if "X" == "X%EC618_RNDIS%" set EC618_RNDIS=disable
+
+@rem 检查SDK
+if exist "%APP_ROOT_PATH%\..\sdk\xmake.lua" goto :CheckSdkOk
+echo SDK不存在,请使用git下载并使用git submodule update --init下载子模块。
+pause
+goto :eof
+:CheckSdkOk
+set ROOT_PATH=%APP_ROOT_PATH%\..\sdk
+set ROOT_PATH=%ROOT_PATH:\=/%
+
+@rem 检查xmake
+xmake --version 2> nul  > nul
+if "X%ERRORLEVEL%" == "X0" goto :CheckXMakeOk
+echo xmake 未安装，请到https://xmake.io下载安装
+pause
+goto :eof
+:CheckXMakeOk
+
+
+@rem 清理工程目录
+echo SDK:%ROOT_PATH:/=\%,APP:%APP_ROOT_PATH%
+pushd %ROOT_PATH:/=\%
+xmake clean --all -v
+popd
