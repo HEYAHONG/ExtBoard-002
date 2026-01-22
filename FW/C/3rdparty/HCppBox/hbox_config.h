fc0780d8101bc8a222f923c8f255f9611739ee7c
HEYAHONG <2229388563@qq.com>
2026-01-22 11:23:43 +0800

    更新HCppBox

--- FW/C/3rdparty/HCppBox/hbox_config.h
+++ FW/C/3rdparty/HCppBox/hbox_config.h
@@ -56,5 +56,38 @@
  */
 #define H3RDPARTY_USING_SYSTEM_TLSF 1
 
+#if defined(H3RDPARTY_TLSF_PORT)
+/*
+ * 恢复TLSF默认的定义(SDK中的TLSF头文件某些函数的参数较开源版本多了参数)
+ */
+#include "tlsf.h"
+static inline void * tlsf_malloc1(tlsf_t tlsf,size_t size)
+{
+    return tlsf_malloc(tlsf, size, 0);
+}
+static inline void * tlsf_memalign1(tlsf_t tlsf,size_t align,size_t bytes)
+{
+    return tlsf_memalign(tlsf, align, bytes, 0);
+}
+static inline void * tlsf_realloc1(tlsf_t tlsf,void * ptr,size_t size)
+{
+    return tlsf_realloc(tlsf, ptr, size, 0);
+}
+typedef void (*tlsf_walker1)(void* ptr, size_t size, int used, void* user);
+static inline void tlsf_walk_pool1(pool_t pool, tlsf_walker1 walker, void* user)
+{
+    tlsf_walk_pool(pool,(tlsf_walker)(void *)walker);
+}
+static inline bool  tlsf_check_pool1(pool_t pool)
+{
+    return  tlsf_check_pool(pool,0);
+}
+#define  tlsf_malloc                        tlsf_malloc1
+#define  tlsf_memalign                      tlsf_memalign1
+#define  tlsf_realloc                       tlsf_realloc1
+#define  tlsf_walk_pool                     tlsf_walk_pool1
+#define  tlsf_check_pool                    tlsf_check_pool1
+
+#endif
 
 #endif // __HBOX_CONFIG_H__
--- FW/C/3rdparty/HCppBox/master/hbox/hdefaults/syscall/hdefaults_syscall.h
+++ FW/C/3rdparty/HCppBox/master/hbox/hdefaults/syscall/hdefaults_syscall.h
@@ -26,6 +26,10 @@ typedef  intptr_t (*hdefaults_syscall_function_t)(uintptr_t number,va_list va_li
 hdefaults_syscall_function_t hdefaults_syscall_function_find(uintptr_t number);
 
 
+
+/*
+ * 系统调用包装
+ */
 #include "wrapper/hgettimeofday.h"
 #include "wrapper/hsettimeofday.h"
 #include "wrapper/hgetrandom.h"
@@ -38,6 +42,19 @@ hdefaults_syscall_function_t hdefaults_syscall_function_find(uintptr_t number);
 #include "wrapper/hwrite.h"
 #include "wrapper/hlseek.h"
 #include "wrapper/hopen.h"
+#include "wrapper/hclock_getres.h"
+#include "wrapper/hclock_gettime.h"
+#include "wrapper/hclock_settime.h"
+
+
+
+
+
+/*
+ * hsyscall
+ */
+#include "hsyscall/time/hsyscall_time.h"
+#include "hsyscall/random/hsyscall_random.h"
 
 #ifdef __cplusplus
 }
